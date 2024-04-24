d20700c0cb9c9bb820bda5c7bbb4ca40779861e2
onuoli <otso.nuolioja@edu.lapinamk.fi>
2024-04-24 13:50:59 +0300

    lisätty tiedosto tietokantayhteyden muodostamista varten

---
+++ yhteys.php
@@ -0,0 +1,15 @@
+<?php
+    // muodostetaan yhteys tietokantaan
+    try {
+        $palvelin= "localhost";
+        $tietokanta = "r_autio_oy";
+        $tunnus = "testiuser";
+        $salasana = "salasana12";
+        
+        $yhteys = new PDO("mysql:host=$palvelin;dbname=$tietokanta;charset=utf8", "$tunnus", "$salasana");
+        $yhteys->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        print "<p>Tietokantayhteyden avaaminen onnistui.</p>"; 
+    } catch(PDOException $e) {
+        print "<p>Tietokantayhteyden avaaminen epäonnistui.</p>" . $e -> getMessage();
+    }
+?>
