215d70ce5151c32dc74f687a46595d2d98ec7c03
onuoli <otso.nuolioja@edu.lapinamk.fi>
2024-04-25 18:37:02 +0300

    lisätty väliaikainen linkki uloskirjautumiselle

--- as_sivu.php
+++ as_sivu.php
@@ -6,3 +6,5 @@
     // näille sivuille tulee vielä lisää tarkistuksia virheiden estämiseksi,
     // mutta tällä hetkellä sessio toimii ja näyttää tunnuksen oikein
 ?>
+<br>
+<a href="uloskirjautuminen.php" class="btn btn-danger">Kirjaudu ulos</button>
